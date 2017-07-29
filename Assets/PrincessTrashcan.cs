473669102663a38eddc37b49ba6cedf60bd3af7e
Boivin92 <boivin.92@gmail.com>
2017-07-29 14:38:14 -0400

    QUIPS!

--- Assets/PrincessTrashcan.cs
+++ Assets/PrincessTrashcan.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public enum SpecialType
 {
@@ -15,16 +16,19 @@ public class PrincessTrashcan : JamObject {
 	public const float DISTANCE_TO_INSULT = 5f;
 	public static int numberOfHelpReceived = 0;
 
+	public GameObject textBubble;
+	public Text text;
+
 	private bool isInRangeToPlayer = false;
 	private bool quippedHello = false;
 	public bool helped = false;
-	public string helloQuip = "";
 
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
+	private int frameTimer = 0;
+	private int maxFramesForQuip = 150;
 	// Update is called once per frame
 	void Update () {
 		isInRangeToPlayer = Vector3.Distance (this.transform.position, PlayerController.playerPosition) < DISTANCE_TO_INSULT ? true : false;
@@ -32,20 +36,52 @@ public class PrincessTrashcan : JamObject {
 			PlayerController.princessInRange = this;
 
 		if (isInRangeToPlayer && !quippedHello) {
-			Quip (helloQuip);
+			Quip (QuipsCollection.WelcomeQuips[QuipsCollection.Welcome]);
+			QuipsCollection.Welcome++;
 			quippedHello = true;
 		}
+		UpdateQuip ();
 	}
 
 	public void HelpedWith(SpecialType type)
 	{
 		helped = true;
 		numberOfHelpReceived++;
-		Quip ("Now who's in distress?");
+		switch (type)
+		{
+		case SpecialType.AOE:
+			Quip (QuipsCollection.ReloadedAOE [QuipsCollection.AOE]);
+			QuipsCollection.AOE++;
+			break;
+		case SpecialType.HEAL:
+			Quip (QuipsCollection.ReloadedHealing [QuipsCollection.HEAL]);
+			QuipsCollection.HEAL++;
+			break;
+		case SpecialType.SHIELD:
+			Quip (QuipsCollection.ReloadedArmor [QuipsCollection.ARMOR]);
+			QuipsCollection.ARMOR++;
+			break;
+		case SpecialType.TELEPORT:
+			Quip (QuipsCollection.ReloadedTeleport [QuipsCollection.TELEPORT]);
+			QuipsCollection.TELEPORT++;
+			break;
+		default:
+			break;
+		}
+	}
+		
+	void UpdateQuip()
+	{
+		if (textBubble.activeSelf)
+			frameTimer++;
+		if (frameTimer > maxFramesForQuip)
+			textBubble.SetActive (false);
 	}
 
 	void Quip(string speach)
 	{
-		Debug.Log (speach);
+		textBubble.SetActive (true);
+		frameTimer = 0;
+		text.text = speach;
 	}
 }
---
+++ Assets/Scripts/QuipsCollection.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class QuipsCollection : MonoBehaviour {
+	public static List<string> WelcomeQuips = new List<string> ();
+	public static List<string> ReloadedHealing = new List<string> ();
+	public static List<string> ReloadedAOE = new List<string> ();
+	public static List<string> ReloadedTeleport = new List<string> ();
+	public static List<string> ReloadedArmor = new List<string> ();
+	public static bool initiated = false;
+	public static int Welcome = 0;
+	public static int AOE = 0;
+	public static int HEAL = 0;
+	public static int TELEPORT = 0;
+	public static int ARMOR = 0;
+
+
+	void Start()
+	{
+		WelcomeQuips.Add (@"Hey, if you're bad, I can help you.");
+		WelcomeQuips.Add (@"I only listen to trash music.");
+		WelcomeQuips.Add (@"If you had an inventory I'd sell you stuff.");
+		WelcomeQuips.Add (@"You again? I guess bad company is better than alone.");
+		WelcomeQuips.Add (@"One man's trash is another princess's.... trash?");
+
+		ReloadedHealing.Add (@"Yeah you seem to need it.");
+		ReloadedHealing.Add (@"That again? Okay.");
+		ReloadedHealing.Add (@"Who's in distress now?");
+		ReloadedHealing.Add (@"If you make it through I'll unlock easy mode for you.");
+		ReloadedHealing.Add (@"This is getting humiliating for you...");
+
+		ReloadedAOE.Add (@"Blow them up!");
+		ReloadedAOE.Add (@"Soooo.... did you miss? Is that why you pick it up again?");
+		ReloadedAOE.Add (@"You could just attack regularly for once.");
+		ReloadedAOE.Add (@"You know this isn't your only power, right?");
+		ReloadedAOE.Add (@"This is getting humiliating for you...");
+
+		ReloadedTeleport.Add (@"When in a though spot...");
+		ReloadedTeleport.Add (@"A bit shy, I see.");
+		ReloadedTeleport.Add (@"Have you tried fighting?");
+		ReloadedTeleport.Add (@"You know, you can't win by running away.");
+		ReloadedTeleport.Add (@"This is getting humiliating for you...");
+
+		ReloadedArmor.Add (@"Always use protection.");
+		ReloadedArmor.Add (@"I said always but I didn't mean it that much.");
+		ReloadedArmor.Add (@"This won't save you all the time.");
+		ReloadedArmor.Add (@"This is not how you're supposed to do it.");
+		ReloadedArmor.Add (@"This is getting humiliating for you...");
+
+		initiated = true;
+	}
+}
