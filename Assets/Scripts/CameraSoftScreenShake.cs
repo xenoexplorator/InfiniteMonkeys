2ac3014a72481a84f57493a4236568ca17774800
Boivin92 <boivin.92@gmail.com>
2017-07-29 15:53:29 -0400

    main menu started -yo

---
+++ Assets/Scripts/CameraSoftScreenShake.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSoftScreenShake : MonoBehaviour {
+
+	public Camera camera; // set this via inspector
+	public float shake = 0;
+	public float shakeAmount = 0.7f;
+
+	void Update() {
+		var test = Random.insideUnitSphere * shakeAmount;
+		camera.transform.localPosition = Random.insideUnitSphere * shakeAmount;
+	}
+
+}
