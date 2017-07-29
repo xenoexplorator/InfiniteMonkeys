45dae3a4974236c93514cfd24bd60ea25c1d2cb0
Boivin92 <boivin.92@gmail.com>
2017-07-28 23:31:16 -0400

    Specials logic starting to take form

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerController : JamObject {
 
+	public const int MAX_HEALTH = 100;
+
 	private  float verticalSpeed = 0;
 	private float horizontalSpeed = 0;
 	private int wKey = 0;
@@ -11,9 +13,13 @@ public class PlayerController : JamObject {
 	private int sKey = 0;
 	private int dKey = 0;
 
+	private int health = MAX_HEALTH;
+
+	private float baseSpeed;
+
 	// Use this for initialization
 	void Start () {
-		
+		baseSpeed = speed;
 	}
 
 	// Update is called once per frame
@@ -23,6 +29,7 @@ public class PlayerController : JamObject {
 		aKey = Input.GetKey (KeyCode.A) ? 1 : 0;
 		sKey = Input.GetKey (KeyCode.S) ? 1 : 0;
 		dKey = Input.GetKey (KeyCode.D) ? 1 : 0;
+		speed = Input.GetKey (KeyCode.LeftShift) ? baseSpeed * 2 : baseSpeed;
 
 		verticalSpeed = wKey - sKey;
 		horizontalSpeed = dKey - aKey;
@@ -31,5 +38,41 @@ public class PlayerController : JamObject {
 			direction = (Mathf.Atan2 (verticalSpeed, horizontalSpeed)) * 180 / Mathf.PI;
 			Move ();
 		}
+
+	}
+
+	void Attack()
+	{
+		//Who knows what will happen here. Not me, at least.
+	}
+
+	public bool ShieldAvailable = true;
+	public bool ShieldUp = false;
+	void SpecialShield()
+	{
+		ShieldAvailable = false;
+		ShieldUp = true;
+	}
+		
+	public void TakeDamage(int dmg)
+	{
+		health -= dmg;
+		if (ShieldUp) {
+			ShieldUp = false;
+			return;
+		}
+		if (health <= 0)
+			GameOver ();
+	}
+
+	private bool FullHealAvailable = true;
+	void SpecialFullHeal()
+	{
+		health = MAX_HEALTH;
+		FullHealAvailable = false;
+	}
+
+	public void GameOver()
+	{
 	}
 }
