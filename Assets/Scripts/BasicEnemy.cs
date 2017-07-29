ba7a8f92bd5c99a5606a8b0f9e2010010e23d541
Boivin92 <boivin.92@gmail.com>
2017-07-28 23:57:41 -0400

    Moved things around, started enemy logic

---
+++ Assets/Scripts/BasicEnemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicEnemy : JamObject {
+
+	private float distanceToPlayer;
+	private  bool aggroed = false;
+	private Vector3 startingLocation;
+
+	public float AggroDistance;
+	public int health;
+
+	// Use this for initialization
+	void Start () {
+		startingLocation = this.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		distanceToPlayer = Vector3.Distance (this.transform.position, PlayerController.playerPosition);
+		aggroed = distanceToPlayer < AggroDistance ? true : false;
+	}
+}
