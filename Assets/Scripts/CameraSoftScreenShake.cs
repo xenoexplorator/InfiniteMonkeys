ac4bdb185354371aa4aa41919188878ecff94737
Boivin92 <boivin.92@gmail.com>
2017-07-29 16:04:50 -0400

    screenshake main menu

--- Assets/Scripts/CameraSoftScreenShake.cs
+++ Assets/Scripts/CameraSoftScreenShake.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class CameraSoftScreenShake : MonoBehaviour {
 
-	public Camera camera; // set this via inspector
+	public GameObject something; // set this via inspector
 	public float shake = 0;
 	public float shakeAmount = 0.7f;
 
 	void Update() {
 		var test = Random.insideUnitSphere * shakeAmount;
-		camera.transform.localPosition = Random.insideUnitSphere * shakeAmount;
+		something.transform.localPosition = Random.insideUnitSphere * shakeAmount;
 	}
 
 }
