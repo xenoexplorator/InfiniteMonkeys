69b6697688ac95b8a842139b55ff3a37c8f7c4e1
xenoexplorator <cedric.pinard@gmail.com>
2017-07-29 22:30:17 -0400

    Finish pacifist layout, fix roof tile hole

--- Assets/Scripts/DungeonParser.cs
+++ Assets/Scripts/DungeonParser.cs
@@ -21,15 +21,14 @@ public class DungeonParser : MonoBehaviour {
 	public void Update() { }
 
 	public void Parse() {
-		var y = 0f - 8*step;
+		var y = 0f;
 		var lines = Fichier.text.Split(new[] { "\r\n", "\n" }, StringSplitOptions.None);
 		foreach (var line in lines) {
 			y += step;
-			float x = 0f - 8*step;
+			float x = 0f;
 			foreach (char c in line) {
 				x += step;
-				if (c == ' ') continue; // empty tile
-				else if (c == '0') { // player spawn
+				if (c == '0') { // player spawn
 					Instantiate(TileMap[4], new Vector3(x, -y, 0), Quaternion.identity);
 					player.transform.position = new Vector3(x, -y, 0);
 				} else if (c >= '1' && c <= '2') { // princess & enemy spawns
@@ -37,7 +36,7 @@ public class DungeonParser : MonoBehaviour {
 					Instantiate(Spawns[c - '1'], new Vector3(x, -y, 0), Quaternion.identity);
 				} else {
 					var id = Array.IndexOf(TileIDs, c);
-					if (id == -1) Debug.Log(c);
+					if (id == -1) continue;
 					// File line numbers and Unity y-coordinate are in opposite directions
 					Instantiate(TileMap[id], new Vector3(x, -y, 0), Quaternion.identity);
 				}
